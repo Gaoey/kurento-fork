bf4450e77d2389b2e65cf1c2fdf944d036ae514e
Jeff Gaoey <gaoey@hotmail.com>
2019-03-01 10:40:12 +0700

    update kurento permission

--- kurento-one2one-call-recording/src/main/resources/static/js/index.js
+++ kurento-one2one-call-recording/src/main/resources/static/js/index.js
@@ -382,16 +382,16 @@ function onIceCandidate(candidate) {
 
 function showSpinner() {
 	for (var i = 0; i < arguments.length; i++) {
-		arguments[i].poster = './img/transparent-1px.png';
-		arguments[i].style.background = 'center transparent url("./img/spinner.gif") no-repeat';
+//		arguments[i].poster = './img/transparent-1px.png';
+//		arguments[i].style.background = 'center transparent url("./img/spinner.gif") no-repeat';
 	}
 }
 
 function hideSpinner() {
 	for (var i = 0; i < arguments.length; i++) {
-		arguments[i].src = '';
-		arguments[i].poster = './img/webrtc.png';
-		arguments[i].style.background = '';
+//		arguments[i].src = '';
+//		arguments[i].poster = './img/webrtc.png';
+//		arguments[i].style.background = '';
 	}
 }
 
